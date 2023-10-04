d90bc68874d8946e88f9f01a00baaaca710b8cb9
msRobiya <ms.robiya2004@gmail.com>
2023-10-05 00:54:59 +0500

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	modified:   scrip.js

--- scrip.js
+++ scrip.js
@@ -26,5 +26,18 @@ window.addEventListener('DOMContentLoaded', function() {
         document.body.scrollTop = 0;
         document.documentElement.scrollTop = 0;
     })
+    window.addEventListener('scroll', () => {
+        let reveals = document.querySelectorAll('.reveal')
+
+        for (let i = 0; i < reveals.length; i++) {
+            let windowHeight = window.innerHeight;
+            let revealTop = reveals[i].getBoundingClientRect().top;
+            let revealPoint = 50;
+
+            if (revealTop < windowHeight - revealPoint) {
+                reveals[i].classList.add('active')
+            }
+        }
+    })
 
 })
